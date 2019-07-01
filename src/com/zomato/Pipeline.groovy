930a7fcb5d34d9b14bfd2e494eda14c6a3d11e05
Vinoth Kumar <vinoth.kumar@zomato.com>
2019-07-01 09:20:47 +0530

    helm test dry-run fix

--- src/com/zomato/Pipeline.groovy
+++ src/com/zomato/Pipeline.groovy
@@ -40,7 +40,7 @@ def helmDeploy(Map args) {
     if (args.dry_run) {
         println "Running dry-run deployment"
 
-        sh "helm upgrade --dry-run --install ${args.name} ${args.chart_dir} --set imageTag=${args.version_tag},replicas=${args.replicas},cpu=${args.cpu},memory=${args.memory} --namespace=${namespace}"
+        sh "helm install --dry-run --debug ${args.chart_dir} --set imageTag=${args.version_tag},replicas=${args.replicas},cpu=${args.cpu},memory=${args.memory} --namespace=${namespace}"
     } else {
         println "Running deployment"
 
